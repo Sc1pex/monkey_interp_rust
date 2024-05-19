2a91fcb3bbd7035453c44bf9f5d1657e4a80e484
Sc1pex <scipex.dev@gmail.com>
2024-05-19 11:08:27 +0300

    eval test macro

--- src/eval/test.rs
+++ src/eval/test.rs
@@ -1,32 +1,30 @@
 use super::*;
 use crate::{ast::Parser, lexer::Lexer};
 
+macro_rules! test {
+    ($($case:expr),* $(,)?) => {
+        test(&[$($case),*])
+    };
+}
+
 #[test]
 fn eval_int() {
-    let inputs = [("5", Object::Integer(5)), ("20", Object::Integer(20))];
-
-    for (inp, exp) in inputs {
-        let lexer = Lexer::new(inp.into());
-        let mut parser = Parser::new(lexer);
-
-        let prog = parser.parse().expect("Skill issue");
-
-        let res = eval_program(prog);
-        assert_eq!(res, exp);
-    }
+    test!(("5", Object::Integer(5)), ("20", Object::Integer(20)));
 }
 
 #[test]
 fn eval_bool() {
-    let inputs = [("true", Object::Bool(true)), ("false", Object::Bool(false))];
+    test!(("true", Object::Bool(true)), ("false", Object::Bool(false)));
+}
 
-    for (inp, exp) in inputs {
-        let lexer = Lexer::new(inp.into());
+fn test(cases: &[(&str, Object)]) {
+    for (inp, exp) in cases {
+        let lexer = Lexer::new(inp.to_string());
         let mut parser = Parser::new(lexer);
 
         let prog = parser.parse().expect("Skill issue");
 
         let res = eval_program(prog);
-        assert_eq!(res, exp);
+        assert_eq!(&res, exp);
     }
 }
